173ed3af8ffd9f1b96e55b2d43b5e525ab6fd63a
Carlos LLanos <cllanoscor@gmail.com>
2025-04-02 17:19:14 +0000

    renderizado en html

--- src/app.js
+++ src/app.js
@@ -5,18 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
- // declarar variables
-const noun = ['pc', 'component', 'domain', 'generator']
-const adj = ['fast', 'tecnic', 'unique', 'dinamic']
-const exten = ['.com', '.es', '.net', '.org', '.io']
+window.onload = function () {
+  // declarar variables
+  const noun = ['pc', 'component', 'domain', 'generator'];
+  const adj = ['fast', 'tecnic', 'unique', 'dinamic'];
+  const exten = ['.com', '.es', '.net', '.org', '.io'];
+  let resultHTML = '';
 
-// funcion de bucle for...in
-for (let i in noun) {
-  for (let j in adj) {
-    for (let k in exten) {
-      console.log('for in', noun[i] + adj[j] + exten[k])
+  // funcion de bucle for...in
+  for (let i in noun) {
+    for (let j in adj) {
+      for (let k in exten) {
+        resultHTML += '<ul>' + noun[i] + adj[j] + exten[k] + '</ul>';
+      }
     }
   }
-}
+
+  document.getElementById('dominios').innerHTML = resultHTML;
 };
+
